f0c8b78cb84f21328fbfe3c7a417554f74af312a
Marko <marko.mp1911@gmail.com>
2017-06-25 12:49:31 +0200

    Def app

---
+++ TurnOffMinute/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TurnOffMinute
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        int sec = 60;
+
+        void PlayTetris()
+        {
+            Console.Beep(658, 125);
+            Console.Beep(1320, 500);
+            Console.Beep(990, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(1188, 250);
+            Console.Beep(1320, 125);
+            Console.Beep(1188, 125);
+            Console.Beep(1056, 250);
+            Console.Beep(990, 250);
+            Console.Beep(880, 500);
+            Console.Beep(880, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(1320, 500);
+            Console.Beep(1188, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(990, 750);
+            Console.Beep(1056, 250);
+            Console.Beep(1188, 500);
+            Console.Beep(1320, 500);
+            Console.Beep(1056, 500);
+            Console.Beep(880, 500);
+            Console.Beep(880, 500);
+            Thread.Sleep(250);
+            /*
+            Console.Beep(1188, 500);
+            Console.Beep(1408, 250);
+            Console.Beep(1760, 500);
+            Console.Beep(1584, 250);
+            Console.Beep(1408, 250);
+            Console.Beep(1320, 750);
+            Console.Beep(1056, 250);
+            Console.Beep(1320, 500);
+            Console.Beep(1188, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(990, 500);
+            Console.Beep(990, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(1188, 500);
+            Console.Beep(1320, 500);
+            Console.Beep(1056, 500);
+            Console.Beep(880, 500);
+            Console.Beep(880, 500);
+            Thread.Sleep(500);
+            Console.Beep(1320, 500);
+            Console.Beep(990, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(1188, 250);
+            Console.Beep(1320, 125);
+            Console.Beep(1188, 125);
+            Console.Beep(1056, 250);
+            Console.Beep(990, 250);
+            Console.Beep(880, 500);
+            Console.Beep(880, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(1320, 500);
+            Console.Beep(1188, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(990, 750);
+            Console.Beep(1056, 250);
+            Console.Beep(1188, 500);
+            Console.Beep(1320, 500);
+            Console.Beep(1056, 500);
+            Console.Beep(880, 500);
+            Console.Beep(880, 500);
+            Thread.Sleep(250);
+            Console.Beep(1188, 500);
+            Console.Beep(1408, 250);
+            Console.Beep(1760, 500);
+            Console.Beep(1584, 250);
+            Console.Beep(1408, 250);
+            Console.Beep(1320, 750);
+            Console.Beep(1056, 250);
+            Console.Beep(1320, 500);
+            Console.Beep(1188, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(990, 500);
+            Console.Beep(990, 250);
+            Console.Beep(1056, 250);
+            Console.Beep(1188, 500);
+            Console.Beep(1320, 500);
+            Console.Beep(1056, 500);
+            Console.Beep(880, 500);
+            Console.Beep(880, 500);
+            Thread.Sleep(500);
+            Console.Beep(660, 1000);
+            Console.Beep(528, 1000);
+            Console.Beep(594, 1000);
+            Console.Beep(495, 1000);
+            Console.Beep(528, 1000);
+            Console.Beep(440, 1000);
+            Console.Beep(419, 1000);
+            Console.Beep(495, 1000);
+            Console.Beep(660, 1000);
+            Console.Beep(528, 1000);
+            Console.Beep(594, 1000);
+            Console.Beep(495, 1000);
+            Console.Beep(528, 500);
+            Console.Beep(660, 500);
+            Console.Beep(880, 1000);
+            Console.Beep(838, 2000);
+            Console.Beep(660, 1000);
+            Console.Beep(528, 1000);
+            Console.Beep(594, 1000);
+            Console.Beep(495, 1000);
+            Console.Beep(528, 1000);
+            Console.Beep(440, 1000);
+            Console.Beep(419, 1000);
+            Console.Beep(495, 1000);
+            Console.Beep(660, 1000);
+            Console.Beep(528, 1000);
+            Console.Beep(594, 1000);
+            Console.Beep(495, 1000);
+            Console.Beep(528, 500);
+            Console.Beep(660, 500);
+            Console.Beep(880, 1000);
+            Console.Beep(838, 2000);*/
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PlayTetris();
+            StartTimer();
+        }
+
+        public void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tick;
+            timer.Start();
+        }
+
+        public void tick(object sender, EventArgs e)
+        {
+            if (sec < 1)
+            {
+                Process.Start("shutdown", "/s /t 0");
+            }
+            //Console.Beep(500,75);// freq, lenght
+            //.Beep();
+            TextBlock_Seconds.Text = sec.ToString();
+            sec--;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            this.Close();
+        }
+
+        private void ShutDown(object sender, RoutedEventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+    }
+}
